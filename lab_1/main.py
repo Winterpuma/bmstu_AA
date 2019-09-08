eee705b3b5945b8eccf5fb5175ea31fb5ed0c9b2
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-09-08 22:00:57 +0300

    Add Damerau-Levenshtein distance

--- lab_1/main.py
+++ lab_1/main.py
@@ -43,3 +43,31 @@ def LevTable(str1, str2, output = False):
     return(table[-1][-1])
 
 
+def DamLevRecursion(str1, str2):
+    if str1 ==  '' or str2 == '':
+        return abs(len(str1) - len(str2))
+    forfeit = 0 if (str1[-1] == str2[-1]) else 1
+    res = min(DamLevRecursion(str1, str2[:-1]) + 1,
+              DamLevRecursion(str1[:-1], str2) + 1,
+              DamLevRecursion(str1[:-1], str2[:-1]) + forfeit)
+    if (len(str1) >= 2 and len(str2) >= 2 and str1[-1] == str2[-2] and str1[-2] == str2[-1]):
+        res = min(res, DamLevRecursion(str1[:-2], str2[:-2]) + 1)
+    return res 
+    
+
+def DamLevTable(str1, str2, output = False):
+    len_i = len(str1) + 1
+    len_j = len(str2) + 1
+    table = [[i + j for j in range(len_j)] for i in range(len_i)]
+    
+    for i in range(1, len_i):
+        for j in range(1, len_j):
+            forfeit = 0 if (str1[i - 1] == str2[j - 1]) else 1
+            table[i][j] = min(table[i - 1][j] + 1,
+                              table[i][j - 1] + 1,
+                              table[i - 1][j - 1] + forfeit)
+            if (i > 1 and j > 1) and str1[i-1] == str2[j-2] and str1[i-2] == str2[j-1]:
+                table[i][j] = min(table[i][j], table[i-2][j-2] + 1)
+    if output:        
+        OutputTable(table, str1, str2)
+    return(table[-1][-1])
