1d9da46a43e09651921ed65df170ac930d33a169
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-10-08 02:45:16 +0300

    Add generation of random  matrix

--- lab_2_MatrMult/main.py
+++ lab_2_MatrMult/main.py
@@ -1,7 +1,9 @@
 # АА Лабораторная2 Умножение матриц
 # Стандартное, Винограда, оптимизированный Винограда
 
-def matr_mult(m1, m2):
+from random import randint
+
+def MatrMult(m1, m2):
     r2 = len(m2)
     c1 = len(m1[0])
     if r2 != c1:
@@ -17,6 +19,16 @@ def matr_mult(m1, m2):
             for k in range(c1):
                 res[i][j] += m1[i][k] * m2[k][j]
     return res
+
+def Gen(n):
+    return [[randint(0, 9) for i in range(n)] for j in range(n)]
+
+def OutTbl(table):
+    for i in range(len(table)):
+        for j in range(len(table[i])):
+            print("{0:2}".format(table[i][j]), end = " ")
+        print()
+    print()
 
 m1 = [[1,4], [5, 8]]
 m2 = [[1,4,3], [5, 8, 3]]
