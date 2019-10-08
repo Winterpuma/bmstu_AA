1a42c2868b9f0e480363df87d41eb1404195baf8
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-10-08 23:51:32 +0300

    Add project

---
+++ lab_3_Sort/lab_3_Sort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5] { 0, 5, -3, 2, 0};
+            //Sorting.BubbleSort(a);
+        }
+
+    }
+}
