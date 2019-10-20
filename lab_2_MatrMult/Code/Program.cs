d5ddb3df1bb4ea760630916ea83ab4db7dbb9f42
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-10-21 00:20:06 +0300

    Add time testing

---
+++ lab_2_MatrMult/Code/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_MatrMult
+{
+    class Program
+    {
+        static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            Time(Mult.MultStand, FillMatr, "Stand0.txt");
+            Time(Mult.MultStand, FillMatr, "Stand1.txt", 1);
+            Time(Mult.MultVin, FillMatr, "Vin0.txt");
+            Time(Mult.MultVin, FillMatr, "Vin1.txt", 1);
+            Time(Mult.MultVinOpt, FillMatr, "VinOpt0.txt");
+            Time(Mult.MultVinOpt, FillMatr, "VinOpt1.txt", 1);
+        }
+        
+        static void Time(Func<int[][], int[][], int[][]> multFunc, Func<int, int, int[][]> genFunc, string filename, int odd = 0)
+        {
+
+            int N_REP = 5;
+            Stopwatch stopWatch = new Stopwatch();
+            List<string> lines = new List<string>();
+
+            for (int size = 100 + odd; size <= 1000 + odd; size += 100)
+            {
+                long ts = 0;
+                for (int repetitions = 1; repetitions <= N_REP; repetitions++)
+                {
+                    var a = genFunc(size, size);
+                    var b = genFunc(size, size);
+                    stopWatch.Start();
+
+                    multFunc(a, b);
+
+                    stopWatch.Stop();
+                    ts += stopWatch.Elapsed.Ticks;
+                }
+                lines.Add(size.ToString() + " " + (ts / N_REP).ToString());
+            }
+            File.AppendAllLines(filename, lines);
+        }
+
+        static int[][] FillMatr(int n, int m)
+        {
+            int[][] matr = new int[n][];
+            int[] tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = new int[m];
+                for (int j = 0; j < m; j++)
+                    tmp[j] = rand.Next(1000);
+                matr[i] = tmp;
+            }
+            return matr;
+        }
+    }
+}
