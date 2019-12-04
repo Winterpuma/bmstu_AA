c614378fa9289961bf3e9f001d5200438a1f2b4d
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-12-05 01:25:27 +0300

    Add finished lab

---
+++ lab_5_Conveyor/lab_5_Conveyor/lab_5_Conveyor/Line.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lab_5_Conveyor
+{
+    enum state {finish, empty, ok};
+
+    class Line
+    {
+        int id;
+        int sleepTime;
+
+        Queue<Args> q;
+        Line nextLine;
+        Action<int, Args, int> conveyorAction;
+
+        public Line(int id, int sleepTime, Action<int, Args, int> a)
+        {
+            this.id = id;
+            this.sleepTime = sleepTime;
+            conveyorAction = a;
+            q = new Queue<Args>();
+        }
+
+        public Line(int id, int sleepTime, List<Args> elements, Line nextLine, Action<int, Args, int> a)
+        {
+            this.id = id;
+            this.sleepTime = sleepTime;
+            q = new Queue<Args>(elements);
+            this.nextLine = nextLine;
+            conveyorAction = a;
+        }
+
+        public Line(int id, int sleepTime, Line nextLine, Action<int, Args, int> a)
+        {
+            this.id = id;
+            this.sleepTime = sleepTime;
+            this.nextLine = nextLine;
+            q = new Queue<Args>();
+            conveyorAction = a;
+        }
+
+        public void Run()
+        {
+            state ret = state.ok;
+            while(ret != state.finish)
+            {
+                ret = ProcessElement();
+                if (ret == state.empty)
+                    Thread.Sleep(500);
+            }
+        }
+
+        public state ProcessElement()
+        {
+            Args el = null;
+            lock(q)
+            {
+                if (q.Count > 0)
+                {
+                    el = q.Dequeue();
+                }
+            }
+            
+            if (el != null)
+            {
+                if (el.IsLast())
+                {
+                    if (nextLine != null)
+                        nextLine.AddElement(el);
+                    return state.finish;
+                }
+                conveyorAction(id, el, sleepTime);
+                if (nextLine != null)
+                    nextLine.AddElement(el);
+            }
+            else
+            {
+                return state.empty;
+            }
+
+            return state.ok;
+        }
+
+
+        public void AddElement(Args arg)
+        {
+            lock (q)
+            {
+                q.Enqueue(arg);
+            }
+        }
+    }
+}
--- lab_5_Conveyor/lab_5_Conveyor/lab_5_Conveyor/Program.cs
+++ lab_5_Conveyor/lab_5_Conveyor/lab_5_Conveyor/Program.cs
@@ -14,23 +14,33 @@ namespace lab_5_Conveyor
         {
             int nLines = 3;
             Thread[] t = new Thread[nLines];
-            Conveyor[] c = new Conveyor[nLines];
+            Line[] c = new Line[nLines];
             // Создаем и генерируем данные
             List<Args> allData = new List<Args>();
-            GenData(nLines, 1000, allData);
+            GenData(nLines, 5, allData);
 
             // Создаем конвейеры 
-            c[1] = new Conveyor(allData);
-            for (int i = 1; i < nLines; i++)
+            c[nLines - 1] = new Line(nLines - 1, 100, Act1);
+            for (int i = nLines - 2; i > 0; i--)
             {
-                c[i] = new Conveyor();
+                c[i] = new Line(i, 200, c[i+1], Act1);
             }
-            
+            c[0] = new Line(0, 300, allData, (nLines > 1) ? c[1] : null, Act1);
 
-            /*foreach (Thread thread in t)
+            // Запускаем конвейеры каждый в своем потоке
+            for (int i = 0; i < nLines; i++)
             {
-                thread.Join();//Или закрываем, дожидаясь сигнала?
-            }*/
+                t[i] = new Thread(c[i].Run);
+                t[i].Start();
+            }
+
+            // Ждем завершения всех потоков
+            foreach (Thread thread in t)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("thats all");
 
         }   
 
@@ -40,18 +50,35 @@ namespace lab_5_Conveyor
             {
                 data.Add(new Args(nLines));
             }
+            data.Add(new Args(0, true));
+        }
+
+        static void Act1(int i, Args a, int milliseconds)
+        {
+            a.ts[i] = DateTime.Now.Millisecond;
+            Console.WriteLine(i.ToString() + " start " + a.ts[i].ToString());
+            Thread.Sleep(milliseconds);
+            a.te[i] = DateTime.Now.Millisecond;
+            Console.WriteLine(i.ToString() + " end   " + a.te[i].ToString());
         }
     }
 
     class Args
     {
+        private bool last;
         public long[] ts;
         public long[] te;
 
-        public Args(int n)
+        public Args(int n, bool lastArg = false)
         {
             ts = new long[n];
             te = new long[n];
+            last = lastArg;
+        }
+
+        public bool IsLast()
+        {
+            return last;
         }
     }
 }
