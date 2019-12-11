b77225848f4ff12e548ef10ad805b07bcb9a00c0
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-12-11 21:54:04 +0300

    Add standard algo

---
+++ lab_7_Substr/lab_7_Substr/lab_7_Substr/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_7_Substr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = StrMatching.Standard("aaabaa", "aba");
+        }
+    }
+}
---
+++ lab_7_Substr/lab_7_Substr/lab_7_Substr/StrMatching.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_7_Substr
+{
+    class StrMatching
+    {
+        public static int Standard(string str, string substr)
+        {
+            for (int i = 0; i <= str.Length - substr.Length; i++)
+            {
+                bool correct = true; 
+                for (int j = 0; j < substr.Length && correct; j++)
+                {
+                    if (str[i + j] != substr[j])
+                        correct = false;
+                }
+                if (correct)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
