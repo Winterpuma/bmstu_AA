737488d0bb153da91122731f83e93b22efec4652
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-12-12 02:01:54 +0300

    Add tests for same length of strings

--- lab_7_Substr/lab_7_Substr/lab_7_SubstrTest/UnitTest1.cs
+++ lab_7_Substr/lab_7_Substr/lab_7_SubstrTest/UnitTest1.cs
@@ -50,5 +50,47 @@ namespace lab_7_SubstrTest
                 Assert.AreEqual(iCorrect, res, "str: " + s + " sub: " + sub);
             }
         }
+
+        [TestMethod]
+        public void TestStandardSameLength()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                string s = rand.Next(100, 999).ToString();
+                string sub = rand.Next(100, 999).ToString();
+
+                int iCorrect = s.IndexOf(sub);
+                int res = lab_7_Substr.StrMatching.Standard(s, sub);
+                Assert.AreEqual(iCorrect, res, "str: " + s + " sub: " + sub);
+            }
+        }
+
+        [TestMethod]
+        public void TestKMPSameLength()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                string s = rand.Next(100, 999).ToString();
+                string sub = rand.Next(100, 999).ToString();
+
+                int iCorrect = s.IndexOf(sub);
+                int res = lab_7_Substr.StrMatching.KMP(s, sub);
+                Assert.AreEqual(iCorrect, res, "str: " + s + " sub: " + sub);
+            }
+        }
+
+        [TestMethod]
+        public void TestBMSameLength()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                string s = rand.Next(100, 999).ToString();
+                string sub = rand.Next(100, 999).ToString();
+
+                int iCorrect = s.IndexOf(sub);
+                int res = lab_7_Substr.StrMatching.BM(s, sub);
+                Assert.AreEqual(iCorrect, res, "str: " + s + " sub: " + sub);
+            }
+        }
     }
 }
