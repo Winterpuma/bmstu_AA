259889259d7970f764ffe953cb15dee4bec18646
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-12-19 03:44:42 +0300

    Add finding of all combinations of elements

---
+++ lab_6_Ant/lab_6_Ant/lab_6_Ant/BruteForce.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_6_Ant
+{
+    class BruteForce
+    {
+        
+        public static List<List<T>> GetAllCombinations<T>(IList<T> arr, List<List<T>> res = null, List<T> current = null)
+        {
+            if (res == null)
+                res = new List<List<T>>();
+            if (arr.Count == 0) //если все элементы использованы
+            {
+                res.Add(current);
+                return res;
+            }
+            for (int i = 0; i < arr.Count; i++)
+            {
+                //в цикле для каждого элемента прибавляем его к итоговому массиву, 
+                //создаем новый список из оставшихся элементов, и вызываем эту же функцию рекурсивно с новыми параметрами
+                List<T> lst = new List<T>(arr);
+                lst.RemoveAt(i);
+                List<T> next;
+                if (current == null)
+                    next = new List<T>();
+                else
+                    next = new List<T>(current);
+                next.Add(arr[i]);
+                GetAllCombinations(lst, res, next);
+            }
+            return res;
+        }
+    }
+}
--- lab_6_Ant/lab_6_Ant/lab_6_Ant/Program.cs
+++ lab_6_Ant/lab_6_Ant/lab_6_Ant/Program.cs
@@ -10,7 +10,13 @@ namespace lab_6_Ant
     {
         static void Main(string[] args)
         {
-            var m = new Map(4);
+            //var m = new Map(4);
+            var ho = new int[3];
+            ho[0] = 2;
+            ho[1] = 3;
+            ho[2] = 4;
+            
+            var a = BruteForce.GetAllCombinations(ho);
         }
     }
 }
