40b9c31aecaa13277a7810b8ff7c15cc41509e22
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-12-20 13:44:34 +0300

    Remove unnecessary for

--- lab_6_Ant/lab_6_Ant/lab_6_Ant/AntAlgorithm.cs
+++ lab_6_Ant/lab_6_Ant/lab_6_Ant/AntAlgorithm.cs
@@ -21,40 +21,42 @@ namespace lab_6_Ant
             {
                 ants = InitAnts(map, n);
 
-                for (int i = 0; i < n; i++)
+                // Построение маршрута колонии; n-1 т.к. 1й город известен
+                for (int i = 0; i < n - 1; i++)
                 {
-                    double[][] pheromonesIter = Program.InitMatr(n, (double)0);
-                    for (int j = 0; j < ants.Count(); j++) // цикл по муравьям
+                    double[][] pheromonesIter = Program.InitMatr(n, (double)0); // временная матрица, колония проходит один шаг
+
+                    // Цикл по муравьям
+                    for (int j = 0; j < ants.Count(); j++) 
                     {
                         double sumChance = 0, chance = 0;
                         bool flag = false;
                         Ant curAnt = ants[j];
                         int curCity = curAnt.path.route.Last();
-                        for (int k = 0; k < n; k++)
+                        // Расчет sumChance
+                        for (int sityId = 0; sityId < n; sityId++) 
                         {
-                            if (curAnt.visited[k] == false) // есть непосещенные города
+                            if (curAnt.visited[sityId] == false) // есть непосещенные города
                             {
-                                sumChance += Math.Pow(pheromones[curCity][k], alpha) * Math.Pow(1 / (map.distance[curCity][k]), beta);
+                                sumChance += Math.Pow(pheromones[curCity][sityId], alpha) * Math.Pow(1 / (map.distance[curCity][sityId]), beta); // сумма вероятностей посещения всех городов
                                 flag = true;
                             }
                         }
-                        if (flag)
+                        // Выбор следующего города муравья
+                        double x = r.NextDouble();
+                        int k = 0;
+                        for (; x > 0; k++)
                         {
-                            double x = r.NextDouble();
-                            int k = 0;
-                            for (; (x > 0) && (k < n); k++)
+                            if (curAnt.visited[k] == false)
                             {
-                                if (curAnt.visited[k] == false)
-                                {
-                                    chance = Math.Pow(pheromones[curCity][k], alpha) * Math.Pow(1 / (map.distance[curCity][k]), beta);
-                                    chance /= sumChance;
-                                    x -= chance;
-                                }
+                                chance = Math.Pow(pheromones[curCity][k], alpha) * Math.Pow(1 / (map.distance[curCity][k]), beta); // шанс выбрать город
+                                chance /= sumChance;
+                                x -= chance;
                             }
-                            k--;
-                            ants[j].VisitedTown(k);
-                            pheromonesIter[curCity][k] += Q / (map.distance[curCity][k]);
                         }
+                        k--;
+                        ants[j].VisitedTown(k);
+                        pheromonesIter[curCity][k] += Q / (map.distance[curCity][k]);
                     }
 
                     // Испарение феромонов
